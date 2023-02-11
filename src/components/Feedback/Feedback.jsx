a7243fbbae4cd5dccf78fb786d35f83c2109563e
Sardor <sardor_09062002@mail.ru>
2023-02-11 21:53:22 +0600

    update

--- src/components/Feedback/Feedback.jsx
+++ src/components/Feedback/Feedback.jsx
@@ -4,39 +4,35 @@ import call from "../../assets/icons/call.svg";
 import ins from "../../assets/icons/ins.png";
 import wha from "../../assets/icons/wha.svg";
 
-const feedback = [
-    {
-        icon: call,
-        title: "+77772446712",
-        link: "+77772446712",
-    },
-    {
-        icon: wha,
-        title: "+77772446712 Whatsapp",
-        link: "https://api.whatsapp.com/send/?phone=77772446712&text&type=phone_number&app_absent=0",
-    },
-    {
-        icon: ins,
-        title: "@mag_dianisiya",
-        link: "https://www.instagram.com/mag_dianisiya/",
-    },
-];
-
 const Feedback = () => {
     return (
         <div className={css.whyMe}>
             <p className={css.title}>Частые вопросы</p>
             <div className={css.arrowBlock}>
-                {feedback.map((item, index) => (
-                    <div key={index} className={css.blockAccordion}>
-                        <div className={css.desrc}>
-                            <img src={item.icon} alt="arrow" className={css.arrow} />
-                            <a href={item.link} className={css.des}>
-                                {item.title}
-                            </a>
-                        </div>
+                <div className={css.blockAccordion}>
+                    <div className={css.desrc}>
+                        <img src={call} alt="arrow" className={css.arrow} />
+                        <a target="_blank" href="tel:+77772446712" className={css.des}>
+                            +77772446712
+                        </a>
                     </div>
-                ))}
+                    <div className={css.desrc}>
+                        <img src={wha} alt="arrow" className={css.arrow} />
+                        <a
+                            target="_blank"
+                            href="https://api.whatsapp.com/send/?phone=77772446712&text&type=phone_number&app_absent=0"
+                            className={css.des}
+                        >
+                            +77772446712 Whatsapp
+                        </a>
+                    </div>
+                    <div className={css.desrc}>
+                        <img src={ins} alt="arrow" className={css.arrow} />
+                        <a target="_blank" href="https://www.instagram.com/mag_dianisiya/" className={css.des}>
+                            @mag_dianisiya
+                        </a>
+                    </div>
+                </div>
             </div>
 
             <p className={css.time}>*Результат может отличаться в каждом индивидуальном (отдельном) случае.</p>
