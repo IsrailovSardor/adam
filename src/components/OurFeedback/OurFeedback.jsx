d7e1c83806cde6201d5d938c9388d4810125927b
Sardor <sardor_09062002@mail.ru>
2023-02-13 20:26:37 +0600

    update

--- src/components/OurFeedback/OurFeedback.jsx
+++ src/components/OurFeedback/OurFeedback.jsx
@@ -1,9 +1,26 @@
-import React from "react";
+import React, {useState} from "react";
 import css from "./OurFeedback.module.scss";
 import card from "../../assets/img/car.png";
 import taro from "../../assets/icons/taro.png";
 import ball from "../../assets/icons/ball.png";
 const OurFeedback = () => {
+    const [name, setName] = useState("");
+    const [number, setNumber] = useState("");
+    const [isAct, setIsAct] = useState(false);
+    const handlePrice = (e) => {
+        e.preventDefault();
+        let str = `${"Имя: " + name + " ," + " Номер: " + number + "."}`;
+        const idChat = "-1001749277447";
+        const tg_bot = "6248232008:AAG9u9PDi8GpOzgFvScPrUs4kpZVRESnra8";
+        var url = `https://api.telegram.org/bot${tg_bot}/sendMessage?chat_id=${idChat}&text=${str} `;
+        let api = new XMLHttpRequest();
+        api.open("GET", url, true);
+        api.send();
+        setName("");
+        setNumber("");
+        setIsAct(true);
+    };
+
     return (
         <div className={css.ourFeedback} id="call">
             <div className={css.container}>
@@ -15,9 +32,12 @@ const OurFeedback = () => {
                         <img src={ball} alt="glass" className={css.img3} />
                     </div>
                     <form className={css.form}>
-                        <input type="text" placeholder="Ваше имя" />
-                        <input type="text" placeholder="Ваш номер телефона" />
-                        <button className={css.btn}>Отправить</button>
+                        {isAct && <p className={css.titInput}>Данные отправлены, ждите с вами свяжуться!</p>}
+                        <input type="text" placeholder="Ваше имя" value={name} onChange={(e) => setName(e.target.value)} />
+                        <input type="number" placeholder="Ваш номер телефона" value={number} onChange={(e) => setNumber(e.target.value)} />
+                        <button className={css.btn} disabled={!name} onClick={(e) => handlePrice(e)}>
+                            Отправить
+                        </button>
                     </form>
                 </div>
             </div>
